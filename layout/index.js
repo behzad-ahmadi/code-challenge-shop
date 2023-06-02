b8229e5db04e8efbed8ecc22b7452892d506346f
behzad <bh.ahmady@gmail.com>
2023-06-02 12:11:07 +0330

    add layout

---
+++ layout/index.js
@@ -0,0 +1,15 @@
+import { Box } from '@mui/material';
+import AppBarSection from './appBar';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <AppBarSection />
+
+      {/* Space for fixed appbar */}
+      <Box mt={8}></Box>
+
+      {children}
+    </>
+  );
+}
