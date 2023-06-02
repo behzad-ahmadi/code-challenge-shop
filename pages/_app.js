a7354c1b149c721f7f1ebd73bddbb39c7a12fe2d
behzad <bh.ahmady@gmail.com>
2023-06-02 12:10:24 +0330

    add theme context

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import { ThemeContextProvider } from '@/context/themeContext';
+import Layout from '@/layout';
+import '../assets/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeContextProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeContextProvider>
+  );
 }
