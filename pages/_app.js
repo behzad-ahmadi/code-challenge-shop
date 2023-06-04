18a4115f55993b1772ea4a6b743e24a3a8cdf6f8
behzad <bh.ahmady@gmail.com>
2023-06-04 15:01:51 +0330

    add AppBarProvider & ToastContainer

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
 import { ThemeContextProvider } from '@/context/themeContext';
 import Layout from '@/layout';
 import '../assets/styles/globals.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AppBarProvider } from '@/context/appBarContext';
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeContextProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <AppBarProvider>
+        <ToastContainer />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AppBarProvider>
     </ThemeContextProvider>
   );
 }
