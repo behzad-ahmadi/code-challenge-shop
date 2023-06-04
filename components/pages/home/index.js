66f9820c09052e4f6bece63eeb7dfcadb18151bf
behzad <bh.ahmady@gmail.com>
2023-06-04 20:03:29 +0330

    .

--- components/pages/home/index.js
+++ components/pages/home/index.js
@@ -1,47 +1,34 @@
-import useUser from '@/hooks/useUser';
-import { apiBaseUrl, getProducts, login } from '@/lib/api-utils';
-import axiosError from '@/lib/axiosError';
-import axios from 'axios';
-import { useEffect } from 'react';
-import useSWR from 'swr';
-
-// const fetcher = (url) =>
-//   axios.get(url).then((res) => {
-//     console.log('url', url);
-//     return res.data;
-//   });
+import useAppBar from '@/hooks/useAppBar';
+import { Box, Button } from '@mui/material';
+import { useRouter } from 'next/router';
 
 export default function Home() {
-  // const { user } = useUser();
-  // const { data, error, isLoading } = useSWR('products', () =>
-  //   getProducts({ authToken: 's' })
-  // );
+  const router = useRouter();
+  const appBr = useAppBar();
 
-  // useEffect(() => {
-  //   console.log('productss', data);
-  //   console.log('error', error);
-  //   console.log('isLoading', isLoading);
-  // }, [data, error, isLoading]);
+  const productsHandler = () => {
+    router.push('/products');
+  };
+  const productDetailHandler = () => {
+    // router.push('/products/1');
+    appBr.setTitle('Home');
+  };
 
-  useEffect(() => {
-    // console.log('Home USer', user);
-    (async () => {
-      try {
-        // const result = await getProducts({ authToken: '' });
-        // const user = await login({
-        //   username: 'atuny0',
-        //   password: '9uQFF1Lh',
-        // });
-        // const result = await resources({ authToken: user.token });
-        // console.log('result', user);
-      } catch (error) {
-        const { message, status } = axiosError(error);
-
-        console.log('err message', message);
-        console.log('err status', status);
-      }
-    })();
-  }, []);
-
-  return <>Home</>;
+  return (
+    <Box
+      display={'flex'}
+      flexDirection={'column'}
+      maxWidth={300}
+      mx={'auto'}
+      gap={5}
+      sx={{ textAlign: 'center', mt: 40 }}
+    >
+      <Button onClick={productsHandler} variant='outlined'>
+        Goto Products
+      </Button>
+      <Button onClick={productDetailHandler} variant='outlined'>
+        Goto Product Detail
+      </Button>
+    </Box>
+  );
 }
