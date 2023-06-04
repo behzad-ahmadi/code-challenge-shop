89a9d3474e9c442f4c0fc0bf6d129bd8987d4cb5
behzad <bh.ahmady@gmail.com>
2023-06-04 21:05:41 +0330

    add SearchIcon fc

---
+++ layout/appBar/searchIcon.js
@@ -0,0 +1,12 @@
+import { Search } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
+
+export default function SearchIcon() {
+  return (
+    <>
+      <IconButton>
+        <Search fontSize='medium' />
+      </IconButton>
+    </>
+  );
+}
