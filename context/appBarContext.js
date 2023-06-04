a80df764dbb93b5a055ec9b3b4f1667583cd3b51
behzad <bh.ahmady@gmail.com>
2023-06-04 20:02:19 +0330

    comment showAppBar

--- context/appBarContext.js
+++ context/appBarContext.js
@@ -1,27 +1,30 @@
 import * as React from 'react';
 
 const AppBarContext = React.createContext({
-  showAppBar: () => {}, //true false null
+  // showAppBar: () => {}, //true false null
   title: '', // appBar title. need set '' or any value to show app bar
   setTitle: (title) => {},
 });
 
 export const AppBarProvider = (props) => {
-  const [showAppBar, setShowAppBar] = React.useState(false);
+  // const [showAppBar, setShowAppBar] = React.useState(false);
   const [title, setTitle] = React.useState('');
 
-  const showAppBarHandler = (show) => {
-    if (show == null) return showAppBar;
-    setShowAppBar(show);
-  };
+  // const showAppBarHandler = (show) => {
+  //   if (show == null) {
+  //     console.log('showAppBarHandler', null);
+  //     return showAppBar;
+  //   }
+  //   setShowAppBar(show);
+  // };
 
   const setTitleHandler = (title) => {
-    if (!showAppBar) setShowAppBar(true);
+    // if (!showAppBar) setShowAppBar(true);
     setTitle(title);
   };
 
   const contextValue = {
-    showAppBar: showAppBarHandler,
+    // showAppBar: showAppBarHandler,
     title: title,
     setTitle: setTitleHandler,
   };
