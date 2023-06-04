14abfd231aaf37afa3978494ba64d4102293e085
behzad <bh.ahmady@gmail.com>
2023-06-04 15:53:31 +0330

    .

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -2,7 +2,8 @@ import Login from '@/components/pages/auth/login';
 import useAppBar from '@/hooks/useAppBar';
 
 export default function LoginPage() {
-  useAppBar().setTitle('Login');
+  // useAppBar().setTitle('Login');
+  useAppBar().showAppBar(false);
 
   return <Login />;
 }
