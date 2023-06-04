46bbe439f71527abeee49640a88763c3bd6785fe
behzad <bh.ahmady@gmail.com>
2023-06-04 15:03:19 +0330

    move some code into its component

--- layout/index.js
+++ layout/index.js
@@ -1,13 +1,9 @@
-import { Box } from '@mui/material';
 import AppBarSection from './appBar';
 
 export default function Layout({ children }) {
   return (
     <>
       <AppBarSection />
-
-      {/* Space for fixed appbar */}
-      <Box mt={8}></Box>
 
       {children}
     </>
