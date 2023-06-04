fe924ae1d90eb19cb2b271895a91d79ed6c01f7e
behzad <bh.ahmady@gmail.com>
2023-06-04 14:56:32 +0330

    .

--- components/pages/home/index.js
+++ components/pages/home/index.js
@@ -1,21 +1,39 @@
-import { getProducts, login } from '@/lib/api-utils';
+import useUser from '@/hooks/useUser';
+import { apiBaseUrl, getProducts, login } from '@/lib/api-utils';
 import axiosError from '@/lib/axiosError';
+import axios from 'axios';
 import { useEffect } from 'react';
+import useSWR from 'swr';
+
+// const fetcher = (url) =>
+//   axios.get(url).then((res) => {
+//     console.log('url', url);
+//     return res.data;
+//   });
 
 export default function Home() {
+  // const { user } = useUser();
+  // const { data, error, isLoading } = useSWR('products', () =>
+  //   getProducts({ authToken: 's' })
+  // );
+
+  // useEffect(() => {
+  //   console.log('productss', data);
+  //   console.log('error', error);
+  //   console.log('isLoading', isLoading);
+  // }, [data, error, isLoading]);
+
   useEffect(() => {
+    // console.log('Home USer', user);
     (async () => {
       try {
         // const result = await getProducts({ authToken: '' });
-
-        const user = await login({
-          username: 'atuny0',
-          password: '9uQFF1Lh',
-        });
-
+        // const user = await login({
+        //   username: 'atuny0',
+        //   password: '9uQFF1Lh',
+        // });
         // const result = await resources({ authToken: user.token });
-
-        console.log('result', user);
+        // console.log('result', user);
       } catch (error) {
         const { message, status } = axiosError(error);
 
