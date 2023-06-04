ce77617f3999664b929517580b7e2c8f9e3d8469
behzad <bh.ahmady@gmail.com>
2023-06-04 15:04:04 +0330

    add avatar menu

--- layout/appBar.js
+++ layout/appBar.js
@@ -8,15 +8,40 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Brightness4, Brightness7 } from '@mui/icons-material';
 import { useAppTheme } from '@/hooks/useAppTheme';
+import { Avatar, Menu, MenuItem } from '@mui/material';
+import useUser from '@/hooks/useUser';
+import { logout } from '@/lib/api-utils';
+import { useRouter } from 'next/router';
+import useAppBar from '@/hooks/useAppBar';
 
 export default function AppBarSection() {
   const { toggleMode, theme } = useAppTheme();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const router = useRouter();
+  const { user, mutateUser } = useUser({ redirectTo: '/auth/login' });
+  const appBar = useAppBar();
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const logoutHandler = async () => {
+    mutateUser(await logout());
+  };
+
+  const loginHandler = () => {
+    router.push('/auth/login');
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position='fixed'>
         <Toolbar>
-          <IconButton
+          {/* <IconButton
             size='large'
             edge='start'
             color='inherit'
@@ -24,20 +49,52 @@ export default function AppBarSection() {
             sx={{ mr: 2 }}
           >
             <MenuIcon />
-          </IconButton>
+          </IconButton> */}
 
           <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-            {/* <Link href={'/'}> Main Page</Link> */}
-            {/* <Link href={'/new_todo'}> Todo List</Link> */}
+            {appBar?.title}
           </Typography>
 
           <IconButton onClick={toggleMode} color='inherit'>
             {theme.palette.mode === 'dark' ? <Brightness4 /> : <Brightness7 />}
           </IconButton>
 
-          <Button color='inherit'>Login</Button>
+          <IconButton>
+            <Avatar
+              src={user?.image}
+              onClick={handleMenu}
+              sx={{ backgroundColor: 'gray' }}
+            ></Avatar>
+          </IconButton>
+
+          <Menu
+            id='menu-appbar'
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            {!user?.isLoggedIn && (
+              <MenuItem onClick={loginHandler}>Login</MenuItem>
+            )}
+
+            {user?.isLoggedIn && (
+              <MenuItem onClick={logoutHandler}>Logout</MenuItem>
+            )}
+          </Menu>
         </Toolbar>
       </AppBar>
+
+      {/* Space for fixed appbar */}
+      <Box mt={8}></Box>
     </Box>
   );
 }
