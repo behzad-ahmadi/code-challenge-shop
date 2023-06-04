be70aad3feef1d623be3ea2f2d22bc583204d90b
behzad <bh.ahmady@gmail.com>
2023-06-04 15:00:10 +0330

    add page title

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Home from '@/components/pages/home';
+import useAppBar from '@/hooks/useAppBar';
 import Head from 'next/head';
 
 export default function Home_page() {
+  useAppBar().setTitle('Home');
+
   return (
     <>
       <Head>
