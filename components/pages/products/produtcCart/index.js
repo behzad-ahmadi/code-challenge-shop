f3653bea3702fc6fbcae1e898948f3f7f1067ec7
behzad <bh.ahmady@gmail.com>
2023-06-05 23:13:37 +0330

    ProductCart

--- components/pages/products/produtcCart/index.js
+++ components/pages/products/produtcCart/index.js
@@ -1,13 +1,7 @@
 import * as React from 'react';
-import Dialog from '@mui/material/Dialog';
-import ListItemText from '@mui/material/ListItemText';
-import ListItem from '@mui/material/ListItem';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Slide from '@mui/material/Slide';
 import { ArrowBackIos } from '@mui/icons-material';
 import { Drawer, Typography, useMediaQuery } from '@mui/material';
 import { useTheme } from '@emotion/react';
