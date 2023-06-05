8aa1eaa31353eaca031ac87035899fa67c6c0597
behzad <bh.ahmady@gmail.com>
2023-06-05 23:12:24 +0330

    add ProductCart fc

--- layout/appBar/cart.js
+++ layout/appBar/cart.js
@@ -1,14 +1,27 @@
+import ProductCart from '@/components/pages/products/produtcCart';
 import { ShoppingCartOutlined } from '@mui/icons-material';
 import { Badge, IconButton } from '@mui/material';
+import { useState } from 'react';
 
 export default function Cart({ itemCount }) {
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => setOpen(false);
+
   return (
     <>
-      <IconButton sx={{ ml: 1 }}>
+      <IconButton
+        sx={{ ml: 1 }}
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
         <Badge badgeContent={itemCount} color='error'>
           <ShoppingCartOutlined />
         </Badge>
       </IconButton>
+
+      <ProductCart open={open} onClose={handleClose} />
     </>
   );
 }
