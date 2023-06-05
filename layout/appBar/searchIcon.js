5d9af26e7c53ab684ad7d188aba2158ed7bc7540
behzad <bh.ahmady@gmail.com>
2023-06-05 23:10:52 +0330

    add ProductSearch fc

--- layout/appBar/searchIcon.js
+++ layout/appBar/searchIcon.js
@@ -1,12 +1,26 @@
+import ProductSearch from '@/components/pages/products/productSeatch';
 import { Search } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
+import { useState } from 'react';
 
 export default function SearchIcon() {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
-      <IconButton>
+      <IconButton onClick={handleClickOpen}>
         <Search fontSize='medium' />
       </IconButton>
+
+      <ProductSearch onClose={handleClose} open={open} />
     </>
   );
 }
