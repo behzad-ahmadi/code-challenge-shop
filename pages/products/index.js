c963a29532d3f3ea7e2edc432834293d47b8abc3
behzad <bh.ahmady@gmail.com>
2023-06-07 22:19:09 +0330

    ProductsPage

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,5 +1,17 @@
+import MyHead from '@/components/common/myHead';
 import Products from '@/components/pages/products';
+import { getProducts } from '@/lib/api-utils';
 
-export default function ProductsPage() {
-  return <Products />;
+export default function ProductsPage({ products }) {
+  return (
+    <>
+      <MyHead pageTitle={'Products'} />
+      <Products products={products} />;
+    </>
+  );
 }
+export const getStaticProps = async () => {
+  const products = await getProducts({ authToken: '' });
+
+  return { props: { products }, revalidate: 20 };
+};
