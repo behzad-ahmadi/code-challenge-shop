b45684126f2d9b875b1034190a171d5777989594
behzad <bh.ahmady@gmail.com>
2023-06-07 22:19:00 +0330

    ProductDetailPage

--- pages/products/[id]/index.js
+++ pages/products/[id]/index.js
@@ -1,5 +1,32 @@
+import MyHead from '@/components/common/myHead';
 import ProductDetails from '@/components/pages/products/productDetails';
+import { getProduct, getProducts } from '@/lib/api-utils';
 
-export default function ProductDetailPage() {
-  return <ProductDetails />;
+export default function ProductDetailPage({ product }) {
+  return (
+    <>
+      <MyHead pageTitle={'Product Details'} />
+      <ProductDetails product={product} />
+    </>
+  );
 }
+
+export const getStaticProps = async ({ params }) => {
+  const { id } = params;
+
+  const product = await getProduct({ productId: id });
+
+  return { props: { product }, revalidate: 20 };
+};
+
+export const getStaticPaths = async () => {
+  const { products } = await getProducts({ authToken: '' });
+
+  const ids = products?.map((p) => ({ params: { id: p.id.toString() } }));
+
+  return {
+    paths: ids,
+
+    fallback: 'blocking',
+  };
+};
