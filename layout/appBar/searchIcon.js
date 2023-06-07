2ebb4c03a6e3013add383c15fd8c68081f4d25e6
behzad <bh.ahmady@gmail.com>
2023-06-07 22:20:31 +0330

    useEffect for back button

--- layout/appBar/searchIcon.js
+++ layout/appBar/searchIcon.js
@@ -1,10 +1,12 @@
 import ProductSearch from '@/components/pages/products/productSeatch';
 import { Search } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
-import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 export default function SearchIcon() {
   const [open, setOpen] = useState(false);
+  const router = useRouter();
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -14,13 +16,17 @@ export default function SearchIcon() {
     setOpen(false);
   };
 
+  useEffect(() => {
+    if (open) setOpen(false);
+  }, [router.asPath]);
+
   return (
     <>
       <IconButton onClick={handleClickOpen}>
         <Search fontSize='medium' />
       </IconButton>
 
-      <ProductSearch onClose={handleClose} open={open} />
+      {/* <ProductSearch onClose={handleClose} open={open} /> */}
     </>
   );
 }
