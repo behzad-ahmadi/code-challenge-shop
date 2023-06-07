b3283a0963fa4afd88b7bfa8e2924c53eb18a0b5
behzad <bh.ahmady@gmail.com>
2023-06-07 08:25:42 +0330

    add CartContextProvider

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,18 @@ import '../assets/styles/globals.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AppBarProvider } from '@/context/appBarContext';
+import { CartContextProvider } from '@/context/cartContext';
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeContextProvider>
       <AppBarProvider>
-        <ToastContainer position='top-center' style={{ fontSize: 15 }} />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <CartContextProvider>
+          <ToastContainer position='top-center' style={{ fontSize: 15 }} />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </CartContextProvider>
       </AppBarProvider>
     </ThemeContextProvider>
   );
