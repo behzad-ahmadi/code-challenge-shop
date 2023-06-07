c238266ca1274250bb10bf871ccc193c4b7ca2c4
behzad <bh.ahmady@gmail.com>
2023-06-07 23:19:08 +0330

    redirect to products page

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,17 @@ import Home from '@/components/pages/home';
 import Products from '@/components/pages/products';
 import useUser from '@/hooks/useUser';
 import { pageRoutes } from '@/lib/config';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function HomePage() {
-  useUser({
-    redirectIfFound: true,
-    redirectTo: pageRoutes.login,
-  });
+  const router = useRouter();
+  const { user } = useUser();
+
+  useEffect(() => {
+    if (user?.isLoggedIn) router.replace(pageRoutes.products);
+    else router.replace(pageRoutes.login);
+  }, []);
 
   return (
     <>
