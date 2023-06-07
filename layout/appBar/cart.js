35d4841025070f9cb0affd07e49e6670afeee4ec
behzad <bh.ahmady@gmail.com>
2023-06-07 22:21:26 +0330

    badge flash

--- layout/appBar/cart.js
+++ layout/appBar/cart.js
@@ -1,12 +1,33 @@
 import ProductCart from '@/components/pages/products/produtcCart';
+import useCart from '@/hooks/useCart';
 import { ShoppingCartOutlined } from '@mui/icons-material';
 import { Badge, IconButton } from '@mui/material';
-import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 export default function Cart({ badeTitle }) {
   const [open, setOpen] = useState(false);
+  const [color, setColor] = useState('error');
+  const router = useRouter();
+  const cart = useCart();
 
   const handleClose = () => setOpen(false);
+
+  useEffect(() => {
+    setColor('success');
+
+    const timer = setTimeout(() => {
+      setColor('error');
+    }, 400);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cart.totalPrice]);
+
+  useEffect(() => {
+    if (open) setOpen(false);
+  }, [router.asPath]);
 
   return (
     <>
@@ -16,7 +37,7 @@ export default function Cart({ badeTitle }) {
           setOpen(true);
         }}
       >
-        <Badge badgeContent={badeTitle} color='error'>
+        <Badge badgeContent={badeTitle} color={color}>
           <ShoppingCartOutlined />
         </Badge>
       </IconButton>
