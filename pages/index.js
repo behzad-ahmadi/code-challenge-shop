a172443794f80a1de9827c532587d8e0d583a0e7
behzad <bh.ahmady@gmail.com>
2023-06-07 22:56:53 +0330

    redirectIfFound

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 import MyHead from '@/components/common/myHead';
 import Home from '@/components/pages/home';
 import Products from '@/components/pages/products';
+import useUser from '@/hooks/useUser';
+import { pageRoutes } from '@/lib/config';
 
 export default function HomePage() {
+  useUser({
+    redirectIfFound: true,
+    redirectTo: pageRoutes.login,
+  });
+
   return (
     <>
       <MyHead pageTitle={'Home'} />
       {/* <Home /> */}
-      <Products />
     </>
   );
 }
