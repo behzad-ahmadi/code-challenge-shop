18f93dbffd4dba066eccc9856e88428495517f9a
behzad <bh.ahmady@gmail.com>
2023-06-07 08:29:00 +0330

    .

--- components/pages/home/index.js
+++ components/pages/home/index.js
@@ -1,10 +1,10 @@
 import useAppBar from '@/hooks/useAppBar';
 import { Box, Button } from '@mui/material';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
   const router = useRouter();
-  const appBr = useAppBar();
 
   const productsHandler = () => {
     router.push('/products');
@@ -14,6 +14,12 @@ export default function Home() {
     router.push('/products/1');
   };
 
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    console.log('products', ...products);
+  }, [products]);
+
   return (
     <Box
       display={'flex'}
@@ -21,14 +27,17 @@ export default function Home() {
       maxWidth={300}
       mx={'auto'}
       gap={5}
-      sx={{ textAlign: 'center', mt: 40 }}
+      sx={{ textAlign: 'center', mt: 10 }}
     >
       <Button onClick={productsHandler} variant='outlined'>
         Goto Products
       </Button>
+
       <Button onClick={productDetailHandler} variant='outlined'>
         Goto Product Detail
       </Button>
+
+      {products?.map((p) => p?.count)}
     </Box>
   );
 }
--- components/pages/products/index.js
+++ components/pages/products/index.js
@@ -1,3 +1,29 @@
+import useCart from '@/hooks/useCart';
+import { Button } from '@mui/material';
+
 export default function Products() {
-  return <>Products</>;
+  const cart = useCart();
+
+  console.log('c', cart.products);
+  return (
+    <>
+      Products
+      <Button
+        onClick={() => {
+          cart.add({ id: 1 });
+        }}
+        variant='outlined'
+      >
+        ADD
+      </Button>
+      <Button
+        onClick={() => {
+          cart.remove({ id: 1 });
+        }}
+        variant='outlined'
+      >
+        Remove
+      </Button>
+    </>
+  );
 }
