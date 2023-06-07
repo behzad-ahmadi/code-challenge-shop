787f4a88ef2441d4a7713663c973023b95fd110f
behzad <bh.ahmady@gmail.com>
2023-06-07 22:19:19 +0330

    MyHead

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,5 +1,8 @@
+import MyHead from '@/components/common/myHead';
 import Login from '@/components/pages/auth/login';
 
 export default function LoginPage() {
+  <MyHead pageTitle={'Login'} />;
+
   return <Login />;
 }
