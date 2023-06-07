06dc1f7fa01042f4c21a2f371dbd9799e9d3c3fb
behzad <bh.ahmady@gmail.com>
2023-06-07 22:18:50 +0330

    add SearchPage

---
+++ pages/products/search/index.js
@@ -0,0 +1,12 @@
+import MyHead from '@/components/common/myHead';
+import ProductSearch from '@/components/pages/products/productSeatch';
+
+export default function SearchPage() {
+  return (
+    <>
+      <MyHead pageTitle='Search' />
+
+      <ProductSearch />
+    </>
+  );
+}
