c5e9993f1ea36e8c9a6f2c648d5a8c086269522e
behzad <bh.ahmady@gmail.com>
2023-06-07 22:19:36 +0330

    add MyHead

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,10 @@
+import MyHead from '@/components/common/myHead';
 import Home from '@/components/pages/home';
-import Head from 'next/head';
 
 export default function HomePage() {
   return (
     <>
-      <Head>
-        <title>Shop - Index</title>
-        <meta name='description' content='Geart Online Shop' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.png' />
-      </Head>
-
+      <a pageTitle={'Home'} />
       <Home />
     </>
   );
