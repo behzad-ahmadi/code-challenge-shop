36659de2b8fab6c6fc8ee3462e226a1a9c0ed076
behzad <bh.ahmady@gmail.com>
2023-06-08 12:22:37 +0330

    .

--- hooks/useUser.js
+++ hooks/useUser.js
@@ -4,6 +4,7 @@ import useSWR from 'swr';
 import { getUser } from '@/lib/api-utils';
 import { toast } from 'react-toastify';
 import { fetcher } from '@/lib/fetchers';
+import axios from 'axios';
 
 export default function useUser({
   redirectTo = '',
@@ -15,7 +16,9 @@ export default function useUser({
   //   error,
   // } = useSWR('/api/user', fetcher);
   const {
-    data: user,
+    data: {
+      data: { user },
+    },
     mutate: mutateUser,
     error,
   } = useSWR(
@@ -25,11 +28,13 @@ export default function useUser({
         .get(url)
         .then((res) => res)
         .catch((e) => console.log(e)),
-    { refreshInterval: 0, revalidateOnFocus: true }
+    { refreshInterval: 0, revalidateOnFocus: true, errorRetryCount: 3 }
   );
 
   useEffect(() => {
-    if (error) toast('Error in getting user info', { type: 'error' });
+    console.log('us', user);
+    // if (error) toast('Error in getting user info', { type: 'error' });
+    if (error) console.log('Error in getting user info', error);
     // if no redirect needed, just return (example: already on /dashboard)
     // if user data not yet there (fetch in progress, logged in or not) then don't do anything yet
     if (!redirectTo || !user) return;
